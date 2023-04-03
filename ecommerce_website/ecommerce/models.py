f344868a2f2032564adc156442a3a4496e941327
Kiril Kluchnikov <106924554+ManOfPoco@users.noreply.github.com>
2023-04-03 16:58:00 +0300

    Create shop models for ecommerce website

--- ecommerce_website/ecommerce/models.py
+++ ecommerce_website/ecommerce/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class Shop(models.Model):
+    shop_name = models.CharField(_('Shop Name'), max_length=50)
+    city = models.CharField(_('City'), max_length=255)
+    street = models.CharField(_('Street'), max_length=255)
+    phone_number = models.CharField(max_length=12)
+    products = models.ManyToManyField('Product', blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.shop_name}"
