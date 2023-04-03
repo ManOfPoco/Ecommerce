e62f4581f79b483090bd454cc0c02c11c634d431
Kiril Kluchnikov <106924554+ManOfPoco@users.noreply.github.com>
2023-04-03 17:36:31 +0300

    Add Save for later model to the cart

--- ecommerce_website/cart/models.py
+++ ecommerce_website/cart/models.py
@@ -24,3 +24,10 @@ class CartItem(models.Model):
 
     def __str__(self) -> str:
         return f"{self.product.product_name}"
+
+
+class SaveForLater(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='save_for_later')
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='save_for_later')
